9f53492b09dfa32804412c0051b3b13df4e2ed38
CarterStefan <ssttefancarter@gmail.com>
2020-06-30 15:02:01 +0000

    updated code to go back to code academy tutorial

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,26 +1,16 @@
-/*emailjs.sendForm('gmail', 'stefan_contact', '#contact-form')
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
-
-$('#contact-form').on('submit', function(event) {
-    event.preventDefault(); // prevent reload
+function sendMail(contactForm) {
+    emailjs.send("gmail", "stefan_contact", {
+        "fullname": contactForm.fullname.value,
+        "femailaddress": contactForm.emailaddress.value,
+        "projectsummary": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            alert("Thanks for the email, we will be in touch shortly", response);
+        },
+        function(error) {
+            alert("FAILED", error);
+        }
+    );
     
-    var formData = new FormData(this);
-    formData.append('service_id', 'gmail');
-    formData.append('template_id', 'stefan_contact');
-    formData.append('user_id', 'user_SHunjZOb2DE9ddiTrDf0p');
- 
-    $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
-        type: 'POST',
-        data: formData,
-        contentType: false, // auto-detection
-        processData: false // no need to parse formData to string
-    }).done(function() {
-        alert('Your mail is sent!');
-    }).fail(function(error) {
-        alert('Oops... ' + JSON.stringify(error));
-    });
-});*/
+};
