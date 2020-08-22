ed32dffe8c61a5411976b16885a2f266999bf400
CarterStefan <ssttefancarter@gmail.com>
2020-08-22 20:04:53 +0000

    contact form working

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,16 +1,3 @@
-function sendMail(contactForm) {
-    emailjs.send("gmail", "stefan_contact", {
-        "fullname": contactForm.fullname.value,
-        "femailaddress": contactForm.emailaddress.value,
-        "projectsummary": contactForm.projectsummary.value
-    })
-    .then(
-        function(response) {
-            alert("Thanks for the email, we will be in touch shortly", response);
-        },
-        function(error) {
-            alert("FAILED", error);
-        }
-    );
-    
-};
+$( "button" ).click(function() {
+  $( "thank-you" ).show( "slow" );
+});
